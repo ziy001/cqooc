3d9f7ae01708236980a63e186a69cdd1b48346d6
ziy001 <1936893077@qq.com>
2020-12-13 19:22:51 +0800

    2020/12/13 提高windows平台兼容性

--- src/main/java/cqooc/Main.java
+++ src/main/java/cqooc/Main.java
@@ -31,23 +31,23 @@ public class Main {
                 .replaceAll(" ", "");
         Main.packet = new Packet(xsid);
         try {
-//            System.out.println("1: 单个课程"+"\n"+"2: 全部课程: ");
-//            while (SC.hasNextInt()) {
-//                int i = Integer.parseInt(SC.next().replaceAll("\\r", "")
-//                        .replaceAll("\\n", "")
-//                        .replaceAll(" ", ""));
-//                //格式化
-//                if(i == 2) {
-//                    new MainWhile().allWhile();
-//                    return;
-//                }
-//                if(i == 1) {
-//                    new MainSingle().single();
-//                    return;
-//                }
-//                System.out.println("1: 单个课程"+"\n"+"2: 全部课程: ");
-//            }
-            new MainSingle().single();
+            System.out.println("1: 单个课程"+"\n"+"2: 全部课程: ");
+            while (SC.hasNext()) {
+                String s = SC.next().replaceAll("\\r", "")
+                        .replaceAll("\\n", "")
+                        .replaceAll(" ", "");
+                int i = Integer.parseInt(s);
+                //格式化
+                if(i == 2) {
+                    new MainWhile().allWhile();
+                    return;
+                }
+                if(i == 1) {
+                    new MainSingle().single();
+                    return;
+                }
+                System.out.println("1: 单个课程"+"\n"+"2: 全部课程: ");
+            }
         } catch (Exception e) {
             System.err.println("网络请求错误,请检查xsid值和网络是否连接!!!");
             System.err.println(e.getMessage());
